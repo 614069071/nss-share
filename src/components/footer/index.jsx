b1d8a863c5addf8d922eb8e7e90a28e01836aaa6
JinTong <tong.jin@cdatatec.com>
2021-06-10 14:41:54 +0800

    add footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function Footer() {
+  return (
+    <footer className="footer-wrapper">
+      ©Copyright©2021
+      深圳市西迪特科技有限公司-网络安全存储事业部粤ICP备14098317号
+    </footer>
+  );
+}
+
+export default Footer;
