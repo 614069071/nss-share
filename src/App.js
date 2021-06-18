6c89dd6a06df25b352798c390031d4bade872491
JinTong <tong.jin@cdatatec.com>
2021-06-18 18:04:48 +0800

    移动端优化

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
-    // 判断移动端
-    const scrren = window.innerWidth || document.documentElement.offsetWidth;
-    const isMobile = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i) || ('ontouchend' in document.body) || (scrren < 768);
-
     this.state = {
       hasHold: false,//设置了提取码，是否校验通过
       isNoHold: false, //没有设置提取码
-      isMobile,
+      isMobile: this.isMobile(),
       isOver: false //过期
     };
   }
@@ -25,6 +21,19 @@ export default class App extends Component {
     const hasHold = utils.storages.get('hasHold') || false;
 
     this.setState({ hasHold });
+  }
+
+  isMobile = () => {
+    const scrren = window.innerWidth || document.documentElement.offsetWidth;
+    const reg = /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i;
+    const isMobile = !!navigator.userAgent.match(reg) || ('ontouchend' in document.body) || (scrren < 768);
+
+    if (isMobile) {
+      document.body.classList.add('m-body');
+      document.documentElement.classList.add('m-html');
+    }
+
+    return isMobile;
   }
 
   changeHold = () => {
