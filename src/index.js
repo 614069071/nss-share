01d00498cb1116c1f6fa1390f2ea1fd4f62cd0de
JinTong <tong.jin@cdatatec.com>
2021-06-28 14:15:35 +0800

    u

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@/locales';
-import './styles';
+import '@/styles';
 
 ReactDOM.render(
   <React.StrictMode>
