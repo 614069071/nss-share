8c611610ca4be0dcc57b5d2d45d478f82fe9f6f2
JinTong <tong.jin@cdatatec.com>
2021-07-01 11:53:54 +0800

    u

--- src/views/PC/Colle/index.jsx
+++ src/views/PC/Colle/index.jsx
@@ -333,7 +333,7 @@ class Colle extends Component {
             }`}
             onClick={this.previewImageSwitchLeft}
           >
-            <i className="iconfont icon-arrow-right-copy"></i>
+            <i className="iconfont icon-qiehuanzuo"></i>
           </span>
           <span
             className={`player-image-preview-btn right ${
@@ -341,7 +341,7 @@ class Colle extends Component {
             }`}
             onClick={this.previewImageSwitchRight}
           >
-            <i className="iconfont icon-arrow-right"></i>
+            <i className="iconfont icon-qiehuanyou"></i>
           </span>
         </div>
 
