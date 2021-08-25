7e607f7284526fc5490435264fdff60d2aad2f96
JinTong <tong.jin@cdatatec.com>
2021-08-25 14:41:13 +0800

    u

--- src/views/PC/Colle/index.jsx
+++ src/views/PC/Colle/index.jsx
@@ -16,7 +16,6 @@ import "./index.css";
 */
 
 // const previewImagesColle = [
-//   require("./1.jpg").default,
 //   "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
 //   "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
 //   "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
